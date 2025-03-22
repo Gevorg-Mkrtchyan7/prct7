1c7e50ce53e916da4f6b8e079e4d55416a34b552
Gevorg-Mkrtchyan7 <gevorgmkrtchyan744@gmail.com>
2025-03-22 16:53:48 +0400

    tnayin

--- lucky.c
+++ lucky.c
@@ -11,20 +11,44 @@ int main()
 
 	while(num){
 		b = num % 10;
-		if(b == 4){}
+		if(b == 4){
+		goto a;
+		}
 		num /= 10;			
 	}
+	goto end;
+a:
 	num = num1;
 	while(num){
 		b = num % 10;
-		if (b == 7){}
+		if(b == 7){
+		goto b;
+		}
+		num /= 10;
+	}
+	goto end;
+
+b:
+
+	num = num1;
+	while(num){
+		b = num % 10;
+		if (b == 4 || b == 7){
+		}else{
+		goto end;
+		}
 		num /= 10;
 	}
 	
 	printf("It is lucky number \n");
+	goto end2;
 
-a:
+end:
+
+	printf("It isn't luchy number \n");
 	
+end2:
+
 	return 0;
 
 }
